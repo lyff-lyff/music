f150787e1325cfb457adc06cb8049c54a5a650f4
lyff-lyff <83768457+lyff-lyff@users.noreply.github.com>
2022-06-24 14:33:12 +0800

    Add files via upload

---
+++ js/yiqi.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		
+	  
+Dianji++;
+switch(Dianji)
+    {
+        case 1:
+          alert("小姐姐在考虑一下呗");
+            break;
+        case 2:
+          alert("你是我见过的最帅气善良可爱的男孩");
+            break;
+        case 3:
+         alert("一生一世爱你");
+            break;
+        case 4:
+          alert("家务我全干");
+            break;
+        case 5:
+      alert("不藏私房钱");
+            break;
+        case 6:
+           alert("房子写你名");
+            break;
+        case 7:
+         alert("工资全上交");
+            break;
+        default:
+           alert("工资不上交了");
+            break;
+
+    }
+
+	}
+	hao.onclick=function(){
+		alert("小姐姐终于同意了，我爱你");
+	}
+	
+
+}
